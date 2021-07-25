c3808b2e5e4e2792f884559b0fb2ce94cba0b170
Muhammad-Ishtiaque <79860818+Muhammad-Ishtiaque@users.noreply.github.com>
2021-07-25 16:21:42 +0500

    Add files via upload

--- New folder (2)/app.js
+++ New folder (2)/app.js
@@ -1,5 +1,58 @@
+//Question No 1
 
- 
+var items = [
+  { name: "juice", price: 50, quantity: 3 },
+  { name: "cookie", price: 30, quantity: 9 },
+  { name: "shirt", price: 880, quantity: 1 },
+  { name: "pen", price: 100, quantity: 2 },
+];
+
+//for single items
+let singleItem = items.forEach((item, index) => {
+  let items = item.price;
+  console.log("The price of items", index, "is", items);
+});
+
+//total sum of each item
+let total = items.reduce((sum, item) => sum + item.price, 0);
+console.log("The total prices of items are Rs: ", total);
+
+//Question No 2
+
+var StudentInfo = {
+  firstName: "ISHTIAQUE",
+  lastName: "ASHFAQUE",
+  email: "mishtiaque05@gmail.com",
+  password: "90121",
+  age: 21,
+  gender: "male",
+  city: "Karachi",
+  country: "Pakistan",
+};
+
+let fName = StudentInfo.hasOwnProperty("firstName");
+console.log(fName);
+let lName = StudentInfo.hasOwnProperty("lastName");
+console.log(lName);
+let age = StudentInfo.hasOwnProperty("age");
+console.log(age);
+let country = StudentInfo.hasOwnProperty("country");
+console.log(country);
+
+//Question No 3
+
+function Person(first, last, age, id, school) {
+  this.first = first;
+  this.last = last;
+  this.age = age;
+  this.id = id;
+  this.school = school;
+}
+//Create a Person Object
+const Student1 = new Person("Muhammad", "ishtiaque", 22, 55220, "Saylani");
+const Student2 = new Person("hasham", "aadi", 23, 55330, "Axiom");
+console.log(Student1, Student2);
+
 //Question No 4
 
 function CheckPopulation(name, gender, address, education, profession) {
@@ -11,10 +64,10 @@ function CheckPopulation(name, gender, address, education, profession) {
 }
 
 const Peoples = new CheckPopulation(
-"ahsan parwaiz",
+"muhammad ishtiaque",
   "Male",
   "gulbhar",
-  "bs cs",
+  "ic ma",
   "we developer"
 );
 console.log(Peoples);
@@ -24,13 +77,13 @@ function educationData() {
   var select = document.getElementById("education");
   if (localStorage === "") {
     select[localStorage.getItem("save")].selected = true;
-  } else if (select.value === "cs") {
+  } else if (select.value === "ma") {
     localStorage.setItem("save", select.value);
   } else if (select.value === "acf") {
     localStorage.setItem("save", select.value);
   } else if (select.value === "bba") {
     localStorage.setItem("save", select.value);
-  } else if (select.value === "ms") {
+  } else if (select.value === "ic") {
     localStorage.setItem("save", select.value);
   }
 }
@@ -59,3 +112,4 @@ for (var i = 0; i < radio.length; i++) {
     localStorage.getItem("gender");
   }
 }
+ 
